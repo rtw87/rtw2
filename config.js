7b5d5c13abffd893ed7db3f6372098a6fc07ff2d
sappy <rtw@stronzi.org>
2019-05-15 22:09:16 -0400

    yup
    hhtps

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteTitle: 'Maryannes Site', // Site title.
   siteTitleAlt: 'Business', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'rtw1.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://rtw1.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: 'Family Mediation and Counseling', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
