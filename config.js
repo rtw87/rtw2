d59f4eb11272372a9ee4589db69f8e11e176d05e
sappy <rtw@stronzi.org>
2019-05-15 21:39:14 -0400

    yup
    yup

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
+  siteTitle: 'Maryannes Site', // Site title.
   siteTitleAlt: 'Business', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'rtw1.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Family Mediation and Counseling', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
+  disqusShortname: 'rtw', // Disqus shortname.
+  userName: 'rtw',
   userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  userLocation: 'Ottawa, ON, Canada',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © RTW 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff', // Used for setting manifest background color.
+  backgroundColor: '#900C3F', // Used for setting manifest background color.
 }
